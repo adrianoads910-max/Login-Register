c4607a74285e0e092b1f39b9a2066b1a5904319b
Adriano Souza <adrianoads910@gmail.com>
2025-10-27 21:06:15 -0300

    fix BrowserRouter config and gh-pages base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages/login';
 import { RegisterPage } from './pages/register';
 import { HomePage } from './pages/home';
 import { ProfilePage } from './pages/userprofile';
 import { PrivateRoute } from './PrivateRoute';
 import { UserManagementPage } from './pages/admin/UserManagementPage';
-import { AdminRoute } from './AdminRoute'; // ✅ IMPORTANTE
+import { AdminRoute } from './AdminRoute';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-
-        {/* ✅ Rota protegida por autenticação (token) */}
-        <Route
-          path='/profile'
-          element={
-            <PrivateRoute>
-              <ProfilePage />
-            </PrivateRoute>
-          }
-        />
-
-        {/* ✅ Rota protegida somente para ADMIN */}
-        <Route
-          path='/admin/users'
-          element={
-            <AdminRoute>
-              <UserManagementPage />
-            </AdminRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route
+        path='/profile'
+        element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path='/admin/users'
+        element={
+          <AdminRoute>
+            <UserManagementPage />
+          </AdminRoute>
+        }
+      />
+    </Routes>
   );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+// ✅ Corrige caminho base para o GitHub Pages
 export default defineConfig({
-plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: "/Login-Register/", // 👈 EXATAMENTE o nome do repositório no GitHub
 })
